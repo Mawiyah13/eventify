c259ceec09e7b4b58bc13010f5899dd299d77545
Mawiyah13 <mawiyahh3@gmail.com>
2026-02-13 16:13:33 +0530

    updates

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://eventify-jn59.onrender.com/api",
 });
 
 // Attach token automatically to every request
